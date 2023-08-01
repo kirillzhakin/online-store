3c8bdf592d506aa401ef4ef47e2b91d2d7ff6d8a
kirillzhakin <kirillzhakin@mail.ru>
2023-08-01 09:18:00 +0500

    add fix popular product

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,11 @@
 <template>
 	<Cover />
 	<AboutBrand />
+	<PopularProducts />
 </template>
 
 <script setup>
 	import Cover from '@/components/Cover.vue'
 	import AboutBrand from '@/components/AboutBrand.vue'
+	import PopularProducts from '@/components/PopularProducts.vue'
 </script>
